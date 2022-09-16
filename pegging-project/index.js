ad20e15bc7172d6f5e4d5fb9a3de416a0a940667
fthompson23 <112416933+fthompson23@users.noreply.github.com>
2022-09-16 07:41:49 -0500

    Add files via upload

--- pegging-project/index.js
+++ pegging-project/index.js
@@ -10,8 +10,7 @@ class peg{
   constructor(xPos, yPos, typeChoice){
     this.x = xPos;
     this.y = yPos;
-    this.height = 5;
-    this.width = 5;
+    this.radius = 10;
     this.type = typeChoice;
     // Add type docuemtation here when you create more
     // type 0 is the default, blue peg
@@ -21,17 +20,20 @@ class peg{
 // function to draw a peg
 function drawPeg(drawnPeg){
     ctx.beginPath();
-    ctx.arc(drawnPeg.x, drawnPeg.y, 5, 0, 2 * Math.PI, false);
+    ctx.arc(drawnPeg.x, drawnPeg.y, drawnPeg.radius, 0, 2 * Math.PI, false);
     ctx.fillStyle = 'blue';
     ctx.fill();
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2;
     ctx.strokeStyle = '#003300';
     ctx.stroke();
 }
 
 drawPeg(new peg(100,100,0));
-
-
+drawPeg(new peg(130,100,0));
+drawPeg(new peg(160,100,0));
+drawPeg(new peg(190,100,0));
+drawPeg(new peg(220,100,0));
+drawPeg(new peg(250,100,0));
 
 //not used yet
 function addPegs(){
@@ -42,7 +44,6 @@ function addPegs(){
 function drawScreen(){
     drawPegs();
 };
-
 
 console.log(pegs);
 });
